55cd1fae649c5d180cde55828aa2f7a88999ee5a
pnrobinson <peter.robinson@charite.de>
2015-12-07 23:22:29 +0100

    Starting to implement nussinov

---
+++ src/Nussinov.cpp
@@ -0,0 +1,5 @@
+
+
+
+
+#include "Nussinov.h"
---
+++ src/Nussinov.h
@@ -0,0 +1,48 @@
+#ifndef NUSSINOV_H
+#define NUSSINOV_H
+
+/** 
+ * Nussinov.h
+ * A class to perform Nussinov-based alignments of RNA secondary structure.
+ * Still prototyping.
+ * @author Peter Robinson
+ * @version 0.0.1 (7 December 2015)
+ */
+
+
+
+
+/**
+ * \class Nussinov
+ *
+ * \ingroup Folding
+ * (Note, this needs exactly one \defgroup somewhere)
+ *
+ * \brief Class for folding an RNA sequence according to Nussinov
+ *
+ * Right now, FASTA
+ *
+ * \note Attempts at zen rarely work.
+ *
+ * \author (last to touch it) $Author: bv $
+ *
+ * \version $Revision: 0.0.1 $
+ *
+ * \date $Date: 2005/04/14 14:16:20 $
+ *
+ * Contact: peter.robinson@charite.de
+ *
+ * Created on: 9 December 2015
+ *
+ *
+ */
+
+class Nussinov {
+
+
+
+
+};
+
+
+#endif
